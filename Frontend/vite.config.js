db0136a9300b886e7a47792d15d005b73ee5950a
ShrutikaLondhe15 <shrutikalondhe15@gmail.com>
2025-01-18 12:24:23 +0530

    Initialized Destination URLs

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    eslint({
+      failOnError: false,
+      failOnWarning: false,
+    }),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 5000,
+    open: true,
+  },
+  optimizeDeps: {
+    // Disable React Fast Refresh
+    react: { fastRefresh: false },
+  },
+});
