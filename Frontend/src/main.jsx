2867bcff0840c7f742cee7e81810c4aaf8f59bd2
Subendhu-nyk <subendhu.nyk@gmail.com>
2025-04-01 20:28:56 +0530

    minor changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,11 @@
 import { createRoot } from 'react-dom/client';
+
+import ErrorBoundary from './common/ErrorBoundary/ErrorBoundary.jsx';
 import './index.css';
 import App from './App.jsx';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react.js';
-import { store, persistor } from './store/store.js';
-import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );
